536f0e319f0507cf5ad581e7be8903fee204467c
LuisCampelo2 <luisfilipecab@gmail.com>
2025-04-22 14:16:22 -0300

    segundo commit

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,5 +1,5 @@
-export const UserInfo = ({ User }) => (
-  <a className="UserInfo" href={`mailto:${User.email}`}>
-    {User.name}
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
   </a>
 );
